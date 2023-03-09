a0a2a43ba4d0bf67ef43601d64a8e95711531c60
silthice <ryonlim@hotmail.com>
2023-03-09 16:06:33 +0800

    - Project Setup

---
+++ expenseTracker/expenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseTrackerApp.swift
+//  expenseTracker
+//
+//  Created by Giap on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct expenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
